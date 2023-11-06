6536b435d48c685ca42e09b8b7ba0733596dd83e
Chamod Dananjaya <chamodd@embla.asia>
2023-11-06 11:25:06 +0530

    Add projects into data.js

---
+++ Server/data.js
@@ -0,0 +1,47 @@
+const projects = [
+    {
+      "id": 1,
+      "name": "Project Breeze",
+      "revenue": 25000,
+      "status": "not completed"
+    },
+    {
+      "id": 2,
+      "name": "Command Program",
+      "revenue": 5000,
+      "status": "completed"
+    },
+    {
+      "id": 3,
+      "name": "Project Point",
+      "revenue": 35000,
+      "status": "not completed"
+    },
+    {
+      "id": 4,
+      "name": "Project Mecha",
+      "revenue": 9000,
+      "status": "not completed"
+    },
+    { "id": 5, "name": "Program Pad", "revenue": 11000, "status": "completed" },
+    {
+      "id": 6,
+      "name": "Project Synergy",
+      "revenue": 18000,
+      "status": "not completed"
+    },
+    {
+      "id": 7,
+      "name": "Dynamic Program",
+      "revenue": 25000,
+      "status": "not completed"
+    },
+    {
+      "id": 8,
+      "name": "Project Zenen",
+      "revenue": 12000,
+      "status": "completed"
+    }
+];
+
+module.exports = projects;
