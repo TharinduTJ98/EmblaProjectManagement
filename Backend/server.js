fd051a7bf56aeac4a44bedc13601dae21e49fa2b
Maneesha Maduranga <maneesham@embla.asia>
2023-11-06 14:03:07 +0530

    Handle Cors

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,14 @@ const {
 } = require("./controller/ProjectController");
 
 const server = http.createServer((req, res) => {
+
+
+    // Handle Cors
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
   if (req.url == "/api/v1/projects" && req.method == "GET") {
     getAllProjects(req, res);
   } else if (
